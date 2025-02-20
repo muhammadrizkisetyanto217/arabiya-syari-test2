47fbf7639655afe2812ed19a2946f7e0e92ab491
muhammadrizkisetyanto <muhammadrizkisetyanto217@gmail.com>
2025-02-20 21:59:52 +0700

    first commit(1)

--- middleware/cors.go
+++ middleware/cors.go
@@ -8,7 +8,7 @@ import (
 
 func SetupCORS() gin.HandlerFunc {
 	return cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "https://yourfrontend.com"}, // Sesuaikan dengan frontend
+		AllowOrigins:     []string{"http://localhost:3000", "https://quizz-vffc.vercel.app/.com"}, // Sesuaikan dengan frontend
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Content-Type", "Authorization"},
 		AllowCredentials: true,
